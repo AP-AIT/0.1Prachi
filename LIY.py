dde0adc47740361bd3440ffefd67fc62eebddd32
AP-AIT <149565417+AP-AIT@users.noreply.github.com>
2023-12-07 16:15:09 +0530

    Update LIY.py

--- LIY.py
+++ LIY.py
@@ -12,39 +12,55 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 def extract_text_from_image(image_bytes):
     try:
-        # Open the image using PIL
         image = Image.open(io.BytesIO(image_bytes))
-
-        # Use pytesseract to perform OCR on the image
         extracted_text = pytesseract.image_to_string(image, output_type=Output.STRING)
-
         return extracted_text
     except Exception as e:
         return f"Text extraction error: {e}"
 
 def display_images(username, password, target_email, start_date):
-    # ... (your existing code)
+    try:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login(username, password)
+        mail.select("inbox")
+        search_criterion = f'(FROM "{target_email}" SINCE "{start_date.strftime("%d-%b-%Y")}" BEFORE "{(start_date + timedelta(days=1)).strftime("%d-%b-%Y")}")'
+        result, data = mail.uid('search', None, search_criterion)
+        email_ids = data[0].split()
 
-    # Iterate through the email IDs
-    for email_id in email_ids:
-        result, msg_data = mail.uid('fetch', email_id, "(RFC822)")
-        raw_email = msg_data[0][1]
+        for idx, email_id in enumerate(email_ids, start=1):
+            result, msg_data = mail.uid('fetch', email_id, "(RFC822)")
+            raw_email = msg_data[0][1]
+            msg = email.message_from_bytes(raw_email)
 
-        # Parse the raw email content
-        msg = email.message_from_bytes(raw_email)
+            for part in msg.walk():
+                if part.get_content_maintype() == 'image':
+                    image_bytes = part.get_payload(decode=True)
+                    image = Image.open(io.BytesIO(image_bytes))
+                    st.image(image, caption=f'Image {idx}', use_column_width=True)
 
-        # Iterate through email parts
-        for part in msg.walk():
-            if part.get_content_maintype() == 'image':
-                # Extract image data
-                image_bytes = part.get_payload(decode=True)
+                    extracted_text = extract_text_from_image(image_bytes)
+                    st.text(f'Text from Image {idx}: {extracted_text}')
 
-                # Display image using PIL
-                image = Image.open(io.BytesIO(image_bytes))
-                st.image(image, caption=f'Image {idx}', use_column_width=True)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+    finally:
+        mail.logout()
 
-                # Extract text from the image
-                extracted_text = extract_text_from_image(image_bytes)
-                st.text(f'Text from Image {idx}: {extracted_text}')
+# Streamlit app
+st.title("Image Viewer")
 
-# ... (your existing Streamlit code)
+# Get user input through Streamlit
+email_address = st.text_input("Enter your email address:")
+password = st.text_input("Enter your email account password:", type="password")
+target_email = st.text_input("Enter the email address from which you want to view images:")
+start_date = st.text_input("Enter the start date (YYYY-MM-DD):")
+
+# Check if the user has provided all necessary inputs
+if email_address and password and target_email and start_date:
+    # Display images when the user clicks the button
+    if st.button("View Images"):
+        # Display extracted images
+        display_images(email_address, password, target_email, start_date)
+else:
+    st.warning("Please fill in all the required fields.")
